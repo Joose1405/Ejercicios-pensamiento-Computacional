8deda82e488ba64008ea452e78d1a1cb1aa0c6a0
Joose1405 <141763019+Joose1405@users.noreply.github.com>
2023-09-04 17:32:48 -0600

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,35 @@
+'''#print('Hola mundo')
+
+variable1 = 10.1
+variable2 = 200
+
+#entrada = input("Por favor da una entrada: ")
+#print(entrada)
+
+print(str(variable1) + " lo de atras es un string")
+'''
+#Ejercico suma
+texto = "123"
+print(texto + "123")
+
+#Ejercicio 1
+a = int(input())
+b = int(input())
+c = int(input())
+
+print((a*b)+c)
+
+#Ejercicio 2
+a = int(input())
+print(int(a/2))
+
+#Ejercicio 3
+a = int(input())
+b = int(input())
+print(a > b)
+
+#Ejercicio 4
+a = int(input())
+b = int(input())
+c = int(input())
+print(a<b)
